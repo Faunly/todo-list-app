d61727e45b40e748b0219337a54e3ee5e96e771c
faunly <faunlyanderson@gmail.com>
2025-01-03 17:40:54 +0300

    add: try-catch in http.js

--- src/http.js
+++ src/http.js
@@ -1,61 +1,59 @@
 export async function fetchTasksByCategory(title) {
-    const response = await fetch(`https://easydev.club/api/v1/todos?filter=${title}`);
-    const resData = await response.json();
-
-    if (!response.ok) {
-        throw new Error("Error an occurred!");
+    try {
+        const response = await fetch(`https://easydev.club/api/v1/todos?filter=${title}`);
+        return await response.json();
+    } catch {
+        throw new Error();
     }
-
-    return resData;
 }
 
 export async function addTask(title) {
-    const response = await fetch("https://easydev.club/api/v1/todos", {
-        method: "POST",
-        body: JSON.stringify({
-            "isDone": false,
-            "title": `${title}`
-        }),
-        headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
-    const resData = await response.json();
-
-    if (!response.ok) {
-        throw new Error("Error an occurred!");
+    try {
+        const response = await fetch("https://easydev.club/api/v1/todos", {
+            method: "POST",
+            body: JSON.stringify({
+                "isDone": false,
+                "title": `${title}`
+            }),
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        return await response.json();
+    } catch {
+        throw new Error();
     }
-
-    return resData.message;
 }
 
 export async function changeDataTask(id, title, isDone) {
-    const response = await fetch(`https://easydev.club/api/v1/todos/${id}`, {
-        method: "PUT",
-        body: JSON.stringify({
-            "isDone": isDone,
-            "title": `${title}`
-        }),
-        headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
-    const resData = await response.json();
-
-    if (!response.ok) {
-        throw new Error("Error an occurred!");
+    try {
+        const response = await fetch(`https://easydev.club/api/v1/todos/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({
+                "isDone": isDone,
+                "title": `${title}`
+            }),
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        return await response.json();
+    } catch {
+        throw new Error();
     }
-
-    return resData.message;
 }
 
 export async function deleteTask(id) {
-    await fetch(`https://easydev.club/api/v1/todos/${id}`, {
-        method: "DELETE",
-        headers: {
-            'accept': 'application/json',
-        }
-    });
+    try {
+        await fetch(`https://easydev.club/api/v1/todos/${id}`, {
+            method: "DELETE",
+            headers: {
+                'accept': 'application/json',
+            }
+        });
+    } catch {
+        throw new Error();
+    }
 }
